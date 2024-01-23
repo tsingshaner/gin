8a3976835741bd60ac2d3d124c7f3f4bbc8f925f
Jon-a-than <2140145186@qq.com>
2024-01-23 22:26:17 +0800

    ♻️ refactor: refactor base model struct

--- pkg/model/base.go
+++ pkg/model/base.go
@@ -1,5 +1,18 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
 
-type BaseModel = gorm.Model
+	"gorm.io/gorm"
+)
+
+type BaseModel struct {
+	ID uint `gorm:"primarykey"`
+	DateModel
+}
+
+type DateModel struct {
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
